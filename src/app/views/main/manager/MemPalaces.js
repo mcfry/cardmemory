2a46f79ee5ba2836803ae10d6310f841d257c627
mcfry <mcfrenchfri7@yahoo.com>
2019-04-16 15:29:04 -0700

    minor cleanup

--- src/app/views/main/manager/MemPalaces.js
+++ src/app/views/main/manager/MemPalaces.js
@@ -3,11 +3,12 @@ import React from "react";
 import { observable } from 'mobx';
 import { observer, inject } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 
 // Css
 import './Manager.css';
 
-@withRouter @inject((RootStore) => {
+@withRouter @inject(RootStore => {
 	return {
 		User: RootStore.UserStore,
 		Manager: RootStore.ManagerStore
@@ -24,108 +25,111 @@ class MemPalaces extends React.Component {
 	constructor(props) {
 		super(props);
 
-		// Func Binds
-
 		// Refs
-		this.memPalaceForm = React.createRef();
-		this.groupsInput = React.createRef();
+		for (let key of ['memPalaceForm', 'groupsInput'])
+			this[key] = React.createRef();
 	}
 
-	addMemoryPalace() {
+	addMemoryPalace = () => {
 	}
 
-	deleteMemoryPalace() {
+	deleteMemoryPalace = () => {
 	}
 
-	addImage() {
+	addImage = () => {
 	}
 
-	deleteImage() {
+	deleteImage = () => {
 	}
 
-	updateCurrentPalaceName(name) {
+	updateCurrentPalaceName = (name) => () => {
 		this.currentPalaceName = name;
 		this.currentImageIndex = 0;
 	}
 
-	updateCurrentImageIndex(index) {
+	updateCurrentImageIndex = (index) => () => {
 		this.currentImageIndex = index;
 	}
 
-	updateCurrentGroupInput() {
+	updateCurrentGroupInput = () => {
 		if (this.groupsInput.current) {
 			this.props.Manager.memPalacesObj[this.currentPalaceName].groups_to_image_array[this.currentImageIndex] = this.groupsInput.current.value;
 		}
 	}
 
 	render() {
+		const { Manager } = this.props;
+
 		return(
 			<div id="memory-palace-tab" className="tab-content">
-				<div className={"tab-pane fade" + (this.props.Manager.currentSubNav === 'Memory Palaces' ? " active show" : "")}>
+				<div className={classNames("tab-pane", "fade", {"active show": Manager.currentSubNav === 'Memory Palaces'})}>
 
-					{this.props.Manager.memPalacesObj !== null ? (
-						<div className="container">
-							<div className="row">
-								<div className="col-md-2 col-horiz-reduce-pad">
-									<div className="list-group">
-										{Object.keys(this.props.Manager.memPalacesObj).map((key, index) => (
-											<button key={index} className={"list-group-item list-group-item-action" + (key === this.currentPalaceName ? " active" : "")} onClick={this.updateCurrentPalaceName.bind(this, key)}>
-												<span>{key[0].toUpperCase() + key.slice(1)}</span>
-											</button>
-										))}
-									</div>
-
-									<div className="list-group">
-									  <button className="list-group-item list-group-item-action" onClick={this.addMemoryPalace.bind(this)}>
-									 	<i className="fa fa-plus"></i>&nbsp;<span className="align-suit-text">New Palace</span>
-									  </button>
-									  <button className="list-group-item list-group-item-action" onClick={this.deleteMemoryPalace.bind(this)}>
-									 	<i className="fa fa-trash"></i>&nbsp;<span className="align-suit-text">Delete Palace</span>
-									  </button>
-									</div>
+					{Manager.memPalacesObj !== null && <div className="container">
+						<div className="row">
+							<div className="col-md-2 col-horiz-reduce-pad">
+								<div className="list-group">
+									{Object.keys(Manager.memPalacesObj).map((key, index) => (
+										<button key={index} className={"list-group-item list-group-item-action" + (key === this.currentPalaceName ? " active" : "")} 
+											onClick={this.updateCurrentPalaceName(key)}>
+											<span>{key[0].toUpperCase() + key.slice(1)}</span>
+										</button>
+									))}
 								</div>
 
-								<div className="col-md-2 col-horiz-reduce-pad">
-						            <div className="list-group list-group-nopad">
-										{this.props.Manager.memPalacesObj[this.currentPalaceName].image_urls.map((path, index) => (
-											<button key={index} onClick={this.updateCurrentImageIndex.bind(this, index)} className={"list-group-item list-group-item-center list-group-item-action" + (this.currentImageIndex === index ? " active" : "")}>
-												Image {index}
-											</button>
-										))}
-									</div>
-
-									<div className="list-group">
-									  <button className="list-group-item list-group-item-action" onClick={this.addImage.bind(this)}>
-									 	<i className="fa fa-plus"></i>&nbsp;<span className="align-suit-text">New Image</span>
-									  </button>
-									  <button className="list-group-item list-group-item-action" onClick={this.deleteImage.bind(this)}>
-									 	<i className="fa fa-trash"></i>&nbsp;<span className="align-suit-text">Delete Image</span>
-									  </button>
-									</div>
-								</div>
-
-								<div className="col-md-8">
-						            <div className="container">
-
-						            	<div className="row">
-							            	<div className="col-md-8">
-								            	<div className="form-group">
-													<label htmlFor="groups-input">Number of spots you want to use in this image (info hover)</label>
-													<input type="text" className="form-control col-md-1" id="groups-input" value={this.props.Manager.memPalacesObj[this.currentPalaceName].groups_to_image_array[this.currentImageIndex]} onChange={this.updateCurrentGroupInput.bind(this)} ref={this.groupsInput} />
-												</div>
-											</div>
-							            </div>
-
-							            <div className="row">
-							            	<img className="img-fluid" alt="memory-palace" src={"http://0.0.0.0:3001/" + this.props.Manager.memPalacesObj[this.currentPalaceName].image_urls[this.currentImageIndex]}/>
-						            	</div>
-
-						            </div>
-
+								<div className="list-group">
+								  <button className="list-group-item list-group-item-action" onClick={this.addMemoryPalace}>
+								 	<i className="fa fa-plus"></i>&nbsp;<span className="align-suit-text">New Palace</span>
+								  </button>
+								  <button className="list-group-item list-group-item-action" onClick={this.deleteMemoryPalace}>
+								 	<i className="fa fa-trash"></i>&nbsp;<span className="align-suit-text">Delete Palace</span>
+								  </button>
 								</div>
 							</div>
+
+							<div className="col-md-2 col-horiz-reduce-pad">
+					            <div className="list-group list-group-nopad">
+									{Manager.memPalacesObj[this.currentPalaceName].image_urls.map((path, index) => (
+										<button key={index} onClick={this.updateCurrentImageIndex(index)} 
+											className={"list-group-item list-group-item-center list-group-item-action" + (this.currentImageIndex === index ? " active" : "")}>
+											Image {index}
+										</button>
+									))}
+								</div>
+
+								<div className="list-group">
+								  <button className="list-group-item list-group-item-action" onClick={this.addImage}>
+								 	<i className="fa fa-plus"></i>&nbsp;<span className="align-suit-text">New Image</span>
+								  </button>
+								  <button className="list-group-item list-group-item-action" onClick={this.deleteImage}>
+								 	<i className="fa fa-trash"></i>&nbsp;<span className="align-suit-text">Delete Image</span>
+								  </button>
+								</div>
+							</div>
+
+							<div className="col-md-8">
+					            <div className="container">
+
+					            	<div className="row">
+						            	<div className="col-md-8">
+							            	<div className="form-group">
+												<label htmlFor="groups-input">Number of spots you want to use in this image (info hover)</label>
+												<input type="text" className="form-control col-md-1" id="groups-input" 
+													value={Manager.memPalacesObj[this.currentPalaceName].groups_to_image_array[this.currentImageIndex]} 
+													onChange={this.updateCurrentGroupInput} ref={this.groupsInput} />
+											</div>
+										</div>
+						            </div>
+
+						            <div className="row">
+						            	<img className="img-fluid" alt="memory-palace" 
+						            		src={"http://0.0.0.0:3001/" + Manager.memPalacesObj[this.currentPalaceName].image_urls[this.currentImageIndex]}/>
+					            	</div>
+
+					            </div>
+
+							</div>
 						</div>
-					) : ''}
+					</div>}
 
 				</div>
 	        </div>
